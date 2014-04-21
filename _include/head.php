06fb39a321e1c9f259793b16e0d78b21cee619cc
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-04-21 22:29:09 +0200

    Modified Facebook og: meta tags

--- _include/head.php
+++ _include/head.php
@@ -1,8 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta property="og:url" content="http://pierrestoffe.be" />
-<meta property="og:title" content="Pierre Stoffe" />
-<meta property="og:description" content="I’m an Interaction Design student from Huy, Belgium. This website is my way of sharing what I’ve been working on lately." />
-<meta property="og:image" content="http://pierrestoffe.be/_assets/images/social.jpg" />
+<meta property="og:url" content="http://giftt.me" />
+<meta property="og:title" content="Giftt.me" />
+<meta property="og:description" content="Create and share wishlists." />
+<meta property="og:image" content="http://giftt.me/_assets/images/logo.png" />
 <link rel="apple-touch-icon-precomposed" href="/_assets/images/app.png" />
 <link rel="icon" href="/_assets/images/favicon.png" />
 <link rel="image_src" href="/_assets/images/app.png" />
