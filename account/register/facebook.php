f6178d766ccacaa93c08823d4cf454000b37c6c6
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-04-26 22:56:36 +0200

    Fixed Facebook register

--- account/register/facebook.php
+++ account/register/facebook.php
@@ -37,9 +37,8 @@ if($user){
 if($user){
 
 	$friends = $facebook->api('/me/friends');
-	$friends_list = '';
 	foreach($friends['data'] as $friend){
-		$friends_list .= $friend['id'] . ',';
+		$friends_list[] = $friend['id'];
 	}
 
 	$facebook_id = $user_profile['id'];
@@ -136,7 +135,76 @@ if($user){
 		));
 	}
 
-	header("Location:/register/friends?me=".$results['id']."&me_facebook=".$user_profile['id']."&friends=".$friends_list);
+
+	// FRIENDS
+
+	$me = $results['id'];
+	$me_facebook = $user_profile['id'];
+
+	$query = $db->prepare("SELECT id, username, firstname, lastname, facebook_id FROM users WHERE facebook_id is not null");
+	$query->execute();
+	$results = $query->fetchAll();
+
+	// GET ALL USERS WHO REGISTERED WITH FACEBOOK
+	
+	if(!empty($results)){
+		foreach($results as $result){
+			if($result['facebook_id'] != $me){
+				$users_facebook[] = $result;
+			}
+		}
+
+		if(!empty($users_facebook)){
+			
+			// GET ALL FRIENDS WHO REGISTERED WITH FACEBOOK'S ID
+			foreach($friends_list as $friend){
+				if(in_multiarray($friend, $users_facebook)){
+					$users_friends_facebook_id[] = $friend;
+				}
+			}
+
+			if(!empty($users_friends_facebook_id)){
+
+				// GET ALL FRIENDS WHO REGISTERED WITH FACEBOOK
+				foreach($users_friends_facebook_id as $friend){
+					foreach($users_facebook as $user){
+						if($user['facebook_id'] == $friend){
+							$friends_facebook[] = $user;
+						}
+					}
+				}
+				
+				if(!empty($friends_facebook)){
+				
+					// INSERT INTO FOLLOWS DB
+					foreach($friends_facebook as $friend){
+						$query = $db->prepare("SELECT * FROM follows WHERE who = :who AND who2 = :who2");
+						$query->execute(array(
+							'who' => $me,
+							'who2' => $friend['id']
+						));
+
+						if($query->rowCount() > 0){
+							$query = $db->prepare("UPDATE follows SET follow = 1, date = now() WHERE who = :who AND who2 = :who2");
+							$query->execute(array(
+								'who' => $me,
+								'who2' => $friend['id']
+							));
+						}else{
+							$query = $db->prepare("INSERT INTO follows(who, follow, who2) VALUES(:who, :follow, :who2)");
+							$query->execute(array(
+								'who' => $me,
+								'follow' => 1,
+								'who2' => $friend['id']
+							));
+						}
+					}
+				}
+			}
+		}
+	}
+
+	header("Location:/");
 }
 
 ?>
