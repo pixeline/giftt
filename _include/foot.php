ffd6ae080b0599c1f0d6ba23d6068d10a43f4a71
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-05-15 23:16:23 +0200

    Analytics added

--- _include/foot.php
+++ _include/foot.php
@@ -1,2 +1,11 @@
 <script src="/_assets/js/jquery.min.js"></script>
 <script src="/_assets/js/scripts.min.js"></script>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-51055011-1', 'giftt.me');
+	ga('send', 'pageview');
+</script>
