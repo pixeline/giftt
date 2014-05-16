52a6f8ebcbd01638dabcec987493122f245671f9
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-05-16 14:20:54 +0200

    Price fix

--- add/wish/add_do.php
+++ add/wish/add_do.php
@@ -7,10 +7,12 @@ if(isset($_POST['add_wish'])){
 	$wish_origin = htmlspecialchars($_POST['origin']);
 	$wish_price = htmlspecialchars($_POST['price']);
 	$wish_currency = htmlspecialchars($_POST['currency']);
-	if(empty($wish_currency)){
-		$wish_price_full = $wish_price . "$";
-	}else{
-		$wish_price_full = $wish_price . $wish_currency;
+	if(!$empty($wish_price)){
+		if(empty($wish_currency)){
+			$wish_price_full = $wish_price . "$";
+		}else{
+			$wish_price_full = $wish_price . $wish_currency;
+		}
 	}
 	$wish_image = $_FILES['image'];
 	if(!isset($_POST['wishlist'])){
