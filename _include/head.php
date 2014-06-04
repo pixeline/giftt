04320dc6736894c5c0ac4a0eb6cc85d7636fc3a8
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-06-04 14:22:25 +0200

    Favicons #3

--- _include/head.php
+++ _include/head.php
@@ -12,11 +12,11 @@
 <link rel="apple-touch-icon" sizes="120x120" href="/_assets/images/apple-touch-icon-120x120.png">
 <link rel="apple-touch-icon" sizes="76x76" href="/_assets/images/apple-touch-icon-76x76.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/_assets/images/apple-touch-icon-152x152.png">
-<link rel="icon" type="image/png" href="/_assets/images/favicon-196x196.png" sizes="196x196">
-<link rel="icon" type="image/png" href="/_assets/images/favicon-160x160.png" sizes="160x160">
-<link rel="icon" type="image/png" href="/_assets/images/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/_assets/images/favicon-16x16.png" sizes="16x16">
-<link rel="icon" type="image/png" href="/_assets/images/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 <meta name="msapplication-TileColor" content="#7fba42">
 <meta name="msapplication-TileImage" content="/_assets/images/mstile-144x144.png">
 <link rel="stylesheet" href="/_assets/css/style.css">
