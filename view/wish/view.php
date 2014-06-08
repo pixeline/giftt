b2ddbfff5a10b3081381a821e5784ef2674675e4
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-06-08 23:02:19 +0200

    Shutgun fixes #2

--- view/wish/view.php
+++ view/wish/view.php
@@ -24,9 +24,9 @@
 						
 						<?php if(!empty($current_wish['price'])){ ?>
 						<p class="price"><?php if($current_wish['currency'] == "$"){ echo '$'; } echo $current_wish['price']; if($current_wish['currency'] != "$"){ echo $current_wish['currency']; } ?></p>
-							<?php if($is_shotgun && !$mine){ ?>
-						<p class="shotgun">(<?php if($current_shotgun_author == $me['id']){ echo "you are "; }else{ echo "someone is " ; } ?>offering <?php echo $user['firstname']; ?> this gift... and he doesn't know!)</p>
-							<?php } ?>
+						<?php } ?>
+						<?php if($is_shotgun && !$mine){ ?>
+						<p class="shotgun">(<?php if($current_shotgun_author == $me['id']){ echo "you are "; }else{ echo "someone is " ; } ?>secretely offering <?php echo $user['firstname']; ?> this gift)</p>
 						<?php } ?>
 					</div>
 
