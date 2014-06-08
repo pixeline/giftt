838912d72feadedeaab63e4987d06e2fffe974b7
Pierre Stoffe <pierre.stoffe@gmail.com>
2014-06-08 20:32:46 +0200

    Small fixes

--- _include/wishlist_header.php
+++ _include/wishlist_header.php
@@ -4,7 +4,7 @@
 
 	<div class="container-fluid">
 		
-		<a class="user" href="/<?php echo $user['username']; ?>"><img src="/<?php if(!empty($user['picture'])){ echo $user['picture']; }else{ echo '_assets/images/profile/default.jpg';} ?>" alt="<?php echo $user_name; ?>" /></a>
+		<a class="user" href="/<?php echo $user['username']; ?>"><img src="/<?php echo $user['picture']; ?>" alt="<?php echo $user_name; ?>" /></a>
 		<h2><?php echo $user_name; ?></h2>
 		<?php if(!empty($user['description'])){ ?>
 			<p class="description"><?php echo $user['description']; ?></p>
